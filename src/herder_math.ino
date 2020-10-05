ce8975700f949d3fdf94423df69f89063bb5f46d
JK <kuhlmann.jc@freenet.de>
2020-10-05 15:42:00 +0200

    Quadrat hinzugefügt

--- src/herder_math.ino
+++ src/herder_math.ino
@@ -13,6 +13,10 @@ int HerderAdd(int num1, int num2)
     return num1 + num2;
 }
 
+int HerderPower(int num)
+{
+    return num*num;
+}
 
 void setup()
 {
@@ -23,9 +27,13 @@ void loop()
 {
     int num1 = 3;
     int num2 = 4;
+	int num3 = 19;
 
     Serial.print("HerderAdd: ");
     Serial.println(HerderAdd(num1, num2));
+	
+	Serial.print("HerderPower: ");
+    Serial.println(HerderPower(num3));
 
     Serial.println("");
     delay(1000);
